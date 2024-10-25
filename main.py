30b13041e739523032ec0b8a37652d58ddd01c70
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-10-25 19:57:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,66 +13,6 @@ Mahasiswa1 = st.Page(
     title="001 - Rafi Diva Efangga",
     icon=":material/person:",
 )
-Mahasiswa2 = st.Page(
-    "Buku Kating/005_Efi Defiyati.py",
-    title="005 - Efi Defiyati",
-    icon=":material/person:",
-)
-Mahasiswa3 = st.Page(
-    "Buku Kating/006_Dea Amanda",
-    title="006 - Dea Amanda",
-    icon=":material/person:",
-)
-Mahasiswa4 = st.Page(
-    "Buku Kating/033_Tesalonika Hutajulu",
-    title="033 - Tesalonika Hutajulu",
-    icon=":material/person:",
-)
-Mahasiswa5 = st.Page(
-    "Buku Kating/037_Labo Napitupulu.py",
-    title="037 - Labo Napitupulu",
-    icon=":material/person:",
-)
-Mahasiswa6 = st.Page(
-    "Buku Kating/055_Aulia Rizky Syahridafitri.py",
-    title="055 - Aulia Rizky Syahridafitri",
-    icon=":material/person:",
-)
-Mahasiswa7 = st.Page(
-    "Buku Kating/062_Abdillah Fikri Al Pome",
-    title="062 - Abdillah Fikri Al Pome",
-    icon=":material/person:",
-)
-Mahasiswa8 = st.Page(
-    "Buku Kating/074_Lutfia Aisyah Putri",
-    title="074 - Lutfia Aisyah Putri",
-    icon=":material/person:",
-)
-Mahasiswa9 = st.Page(
-    "Buku Kating/075_Aliya Ammara Ananta",
-    title="075 - Aliya Ammara Ananta",
-    icon=":material/person:",
-)
-Mahasiswa10 = st.Page(
-    "Buku Kating/083_Nadya Ratu Anjani.",
-    title="083_Nadya Ratu Anjani.",
-    icon=":material/person:",
-)
-Mahasiswa11 = st.Page(
-    "Buku Kating/084_Aisyah Musfirah.",
-    title="084 - Aisyah Musfirah.",
-    icon=":material/person:",
-)
-Mahasiswa12 = st.Page(
-    "Buku Kating/089_Lia Hana Ichisasmita",
-    title="089 - Lia Hana Ichisasmita",
-    icon=":material/person:",
-)
-Mahasiswa13 = st.Page(
-    "Buku Kating/105_Arielva Simon Siahaan",
-    title="105 - Arielva Simon Siahaan",
-    icon=":material/person:",
-)
 
 
 
@@ -85,7 +25,7 @@ if st.session_state.pindah:
     pg = st.navigation(
         {
             "Halaman Utama": [Homepage],
-            "Buku Kating": [Mahasiswa1, Mahasiswa2, Mahasiswa3, Mahasiswa4, Mahasiswa5, Mahasiswa6, Mahasiswa7, Mahasiswa8, Mahasiswa9, Mahasiswa10, Mahasiswa11, Mahasiswa12,Mahasiswa13],
+            "Buku Kating": [Mahasiswa1],
             "Try Me !!": [KREASI, KREASII],
         }
     )
